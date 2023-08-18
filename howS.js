f3784dfaf8c425b538e14072bd87ba4f99f50961
BMC1-MEDUSAS <130384402+bmc1-medusas@users.noreply.github.com>
2023-08-18 12:31:01 -0700

    Add files via upload

---
+++ howS.js
@@ -0,0 +1,48 @@
+// Ben Macedo 8.18.23 09.50am
+// Dynamic Programming Problems
+//
+// Understanding logarithms in the code as solution finder
+// the purpose of the object file is to create a traveler again using the
+// MEMOIZATION method in the functions in order to get
+// as close as possible to requested information
+//
+// purpose of this obj in particular is have a 
+// Traveler on a 2 dimensional grid moving from the
+// Top-left corner and to travel to bottom right corner
+// and the only option for now is top or down or right
+//
+// in regards to the function used for traveling on a grid
+// with certain dimensions given by user as (m times n)?
+//
+// so the the function would look like this
+// "howSum(m, n)"" whic is the calculator of dimensions of the grid
+// where 'n' is the actual ways of how many options are to travel
+const howSum=(m , n,memo={})=>{
+        // are arguments in the memo?
+    const key =m+ ',' +n;
+    if (key in memo) return memo[key];
+    if(m === 1 && n===1) return 1;
+    if(m === 0 || n===0) return 0;
+    memo[key]=howSum(m-1,n,memo)+howSumer(m,n-1,memo);
+    return memo[key];
+    //return howSum(m-1,n)+howSum(m,n-1);
+};
+console.log(howSum(1,1)); // 1
+console.log(howSum(2,3)); // 3
+console.log(howSum(3,2)); // 3
+console.log(howSum(3,3)); // 6
+console.log(howSum(18,18)) // 2333606220
+//
+//
+// Recipe to the solution and optimization
+//
+// Make it work first
+//      visualize the problem as a tree
+//      impletement the tree using recursive process
+//      test it -as the brute searh  recursive
+//
+// Make it efficient
+//      add a MEMO [memo] object
+//      add a base case to return memo values -fetching-
+//      store return values into the memo
+//----
